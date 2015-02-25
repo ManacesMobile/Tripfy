53c5eeb4c7b25b1dda806f7cfa6aeed6fde14754
eserkalac <eser@esers-Mac-mini.local>
2015-02-25 22:19:53 +0200

    Parse submit
    Parse and Facebook entegration

---
+++ tripfy/tripfy/tripfy/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  tripfy
+//
+//  Created by eser on 25/02/15.
+//  Copyright (c) 2015 tripfy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ tripfyTab/tripfyTab/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  tripfyTab
+//
+//  Created by eser on 25/02/15.
+//  Copyright (c) 2015 tripfy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
